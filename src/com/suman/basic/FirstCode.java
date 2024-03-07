ce4a1ee1d9c9f61af261f415e9d5d91b09e1c90c
suman <suman99999roy@gmail.com>
2024-03-07 19:08:08 +0530

    add fiestcode to print something

---
+++ src/com/suman/basic/FirstCode.java
@@ -0,0 +1,8 @@
+package com.suman.basic;
+
+public class FirstCode {
+	public static void main(String[] args) {
+		System.out.println("hi there this my first code while pushing the code in remote");
+	}
+
+}
